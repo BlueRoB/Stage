18e860fa3e1e295a506eb77b8edc73d60d9de22f
Anas Gammar <anasgammar@gmail.com>
2023-06-01 16:53:36 +0200

    bug fix

--- view/categories.php
+++ view/categories.php
@@ -1,8 +1,7 @@
 <?php
-$category = $_GET['category'] ?? '';
-$subcategory = $_GET['subcategory'] ?? '';
+$category = isset($_GET['category']) ? $_GET['category'] : '';
+$subcategory = isset($_GET['subcategory']) ? $_GET['subcategory'] : '';
 
-// Function to generate HTML for subcategory links
 function generateSubcategoryLink($category, $subcategory, $subcategoryName)
 {
     $subcategoryLink = "category.php?category=$category&subcategory=$subcategory";
@@ -15,10 +14,8 @@ function generateSubcategoryLink($category, $subcategory, $subcategoryName)
     </a>";
 }
 
-// Switch case to generate HTML based on the category and subcategory
 switch ($category) {
     case 'informatique':
-        // Generate HTML for Informatique category and its subcategories
         echo "<h1>Informatique</h1>";
 
         if (empty($subcategory)) {
@@ -28,25 +25,18 @@ switch ($category) {
             echo generateSubcategoryLink('informatique', 'pieces', 'Pièces');
             echo generateSubcategoryLink('informatique', 'reseaux', 'Réseaux');
         } else {
-            // Handle individual subcategories here
             switch ($subcategory) {
                 case 'ordinateurs':
-                    // Generate HTML for Ordinateurs subcategory
                     break;
                 case 'pc-portables':
-                    // Generate HTML for PC portables subcategory
                     break;
                 case 'peripherique':
-                    // Generate HTML for Périphérique subcategory
                     break;
                 case 'pieces':
-                    // Generate HTML for Pièces subcategory
                     break;
                 case 'reseaux':
-                    // Generate HTML for Réseaux subcategory
                     break;
                 default:
-                    // Redirect to notfound.php or display an error message
                     header('Location: notfound.php');
                     exit();
             }
@@ -54,7 +44,6 @@ switch ($category) {
         break;
 
     case 'image-son':
-        // Generate HTML for Image & Son category and its subcategories
         echo "<h1>Image & Son</h1>";
 
         if (empty($subcategory)) {
@@ -63,22 +52,16 @@ switch ($category) {
             echo generateSubcategoryLink('image-son', 'son-numerique', 'Son numérique');
             echo generateSubcategoryLink('image-son', 'hi-fi-enceintes', 'Hi-fi et enceintes');
         } else {
-            // Handle individual subcategories here
             switch ($subcategory) {
                 case 'appareil-photo':
-                    // Generate HTML for Appareil photo subcategory
                     break;
                 case 'television':
-                    // Generate HTML for Télévision subcategory
                     break;
                 case 'son-numerique':
-                    // Generate HTML for Son numérique subcategory
                     break;
                 case 'hi-fi-enceintes':
-                    // Generate HTML for Hi-fi et enceintes subcategory
                     break;
                 default:
-                    // Redirect to notfound.php or display an error message
                     header('Location: notfound.php');
                     exit();
             }
@@ -86,7 +69,6 @@ switch ($category) {
         break;
 
     case 'jeux-loisirs':
-        // Generate HTML for Jeux & Loisirs category and its subcategories
         echo "<h1>Jeux & Loisirs</h1>";
 
         if (empty($subcategory)) {
@@ -94,19 +76,14 @@ switch ($category) {
             echo generateSubcategoryLink('jeux-loisirs', 'accessoires-console', 'Accessoires console');
             echo generateSubcategoryLink('jeux-loisirs', 'jeux-video', 'Jeux Vidéo');
         } else {
-            // Handle individual subcategories here
             switch ($subcategory) {
                 case 'console':
-                    // Generate HTML for Console subcategory
                     break;
                 case 'accessoires-console':
-                    // Generate HTML for Accessoires console subcategory
                     break;
                 case 'jeux-video':
-                    // Generate HTML for Jeux Vidéo subcategory
                     break;
                 default:
-                    // Redirect to notfound.php or display an error message
                     header('Location: notfound.php');
                     exit();
             }
@@ -114,13 +91,10 @@ switch ($category) {
         break;
 
     case 'all':
-        // Generate HTML for all categories
         echo "<h1>Tous les Catégories</h1>";
-        // Generate HTML for all categories here
         break;
 
     default:
-        // Redirect to notfound.php or display an error message
         header('Location: notfound.php');
         exit();
 }
@@ -150,7 +124,7 @@ require_once __DIR__ . '/navbar.php'; ?>
     <div class="row">
         <div class="col-md-4">
             <h2>Informatique</h2>
-            <?php foreach ($subcategories as $subcategory) { ?>
+            <?php foreach ($subcategory as $subcategory) { ?>
                 <a class="sub-category" href="#">
                     <div class="pic">
                         <img src="https://example.com/informatique/<?php echo strtolower(str_replace(' ', '-', $subcategory)); ?>/thumbnail.jpg" alt="<?php echo $subcategory; ?>">
@@ -162,7 +136,7 @@ require_once __DIR__ . '/navbar.php'; ?>
         </div>
         <div class="col-md-4">
             <h2>Image & Son</h2>
-            <?php foreach ($subcategories as $subcategory) { ?>
+            <?php foreach ($subcategory as $subcategory) { ?>
                 <a class="sub-category" href="#">
                     <div class="pic">
                         <img src="https://example.com/image-son/<?php echo strtolower(str_replace(' ', '-', $subcategory)); ?>/thumbnail.jpg" alt="<?php echo $subcategory; ?>">
@@ -174,7 +148,7 @@ require_once __DIR__ . '/navbar.php'; ?>
         </div>
         <div class="col-md-4">
             <h2>Jeux & Loisirs</h2>
-            <?php foreach ($subcategories as $subcategory) { ?>
+            <?php foreach ($subcategory as $subcategory) { ?>
                 <a class="sub-category" href="#">
                     <div class="pic">
                         <img src="https://example.com/jeux-loisirs/<?php echo strtolower(str_replace(' ', '-', $subcategory)); ?>/thumbnail.jpg" alt="<?php echo $subcategory; ?>">
